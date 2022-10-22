f0e08b095879fc7bfb0145ebd9850c8800a5cfc8
Sofiene Khiari <64355269+sofienekhiari@users.noreply.github.com>
2022-10-22 22:38:44 +0200

    Update the test suite to follow the qualifications

--- test_project.py
+++ test_project.py
@@ -5,9 +5,8 @@ from test_data import *
 from project import filter_mentees
 
 
-def test_filter_mentees_en():
-    """Test the filter mentees function
-    in case the language is english"""
+def test_filter_mentees():
+    """Test the filter mentees function"""
     people_list = [
         david_abendroth,
         maria_nacht,
@@ -20,30 +19,5 @@ def test_filter_mentees_en():
         simone_ebersbacher,
         niklas_abt,
     ]
-
-
-def test_filter_mentees_sg():
-    """Test the filter mentees function
-    in case the language is swiss german"""
-    people_list = [
-        david_abendroth,
-        maria_nacht,
-        mario_moeller,
-        simone_ebersbacher,
-        niklas_abt,
-    ]
     assert filter_mentees(people_list, "Swiss German") == [maria_nacht]
-
-
-def test_filter_mentees_nm():
-    """Test the filter mentees function
-    in case the language is non existant
-    in the list"""
-    people_list = [
-        david_abendroth,
-        maria_nacht,
-        mario_moeller,
-        simone_ebersbacher,
-        niklas_abt,
-    ]
     assert len(filter_mentees(people_list, "Japanese")) == 0
